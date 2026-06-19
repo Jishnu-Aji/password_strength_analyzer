9e8525904d44c3b928bc3518007e045ebf6b045b
Jishnu-Aji <jishnumaster16@gmail.com>
2026-06-19 22:45:45 +0530

    first commit

---
+++ password-strength-analyzer.py
@@ -0,0 +1,27 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if re.search(r"[A-Z]", password):
+        score += 1
+    if re.search(r"[a-z]", password):
+        score += 1
+    if re.search(r"\d", password):
+        score += 1
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
+
+password = input("Enter a password: ")
+strength = check_password_strength(password)
+
+print(f"Password Strength: {strength}")
